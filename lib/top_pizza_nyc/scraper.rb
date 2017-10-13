d5a657cf8eb2008c1a20a609e5a0c9fc749d7ad3
Lauren Feld <lauren.feld@aol.com>
2017-10-13 14:52:46 -0400

    Additions to Scraper.rb
    Added comments to my code to better explain what my program is doing. Grabbed 'neighborhood' property from website.

--- lib/top_pizza_nyc/scraper.rb
+++ lib/top_pizza_nyc/scraper.rb
@@ -6,11 +6,14 @@ class BestPizzaNYC::Scraper
   end
 
   def scrape_pizza
-    self.get_page.css("p").each do |pizza_box|
+    self.get_page.css("p").each do |pizza_box| #calling the above get_page method to find all the text in each 'p' tag
       pizza_joint = NewPizza::Pizza.new()
 
-      pizza_joint.address = pizza_box.css("a").text.split('.')[0]
-      pizza_joint.url = pizza_box.css("a").text.split('.')[1,2].join('.')
+      #pizza_joint.name = pizza_box()
+      pizza_joint.neighborhood = pizza_box("a").text #grabs neighborhood from text
+      pizza_joint.address = pizza_box.css("a").text.split('.')[0] #grabs address via #split with a '.' to separate url from the address
+      pizza_joint.url = pizza_box.css("a").text.split('.')[1,2].join('.') || "None Listed" #joining the url array or givng the none listed option.
+
     end
   end
-end 
+end
