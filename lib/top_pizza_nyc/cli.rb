885cc69c6016624b996c2542444dc64cdf091087
Lauren Feld <lauren.feld@aol.com>
2017-10-13 21:56:23 -0400

    Additions to Cli.rb

--- lib/top_pizza_nyc/cli.rb
+++ lib/top_pizza_nyc/cli.rb
@@ -1,20 +1,40 @@
-class BestPizzaNYC::CLI
+class TopPizzaNYC::CLI
 
-  def call_method
-    BestPizzaNYC::Scraper.scrape_pizza
-    puts "The 25 Best Pizza Places in NYC. I hope you brought your appetite!"
-    start_method
+  def call
+    TopPizzaNYC::Scraper.new
+    puts "Welcome to the 25 Best Pizza Places in NYC. I hope you brought your appetite!"
+    start
   end
 
-  def start_method
+  def start
     puts "Which pizza joint would you prefer to see first? 1-5, 5-10, 10-15, 15-20, 20-25?"
-
-
     input = gets.strip.to_i
-
     display_pizza_joint(pizza_joint)
 
     puts "Would you like to see another pizza joint? Enter Y or N"
     input = gets.strip.downcase
+
+    pizza = TopPizzaNYC.find[input.to_i - 1]
+
+  if input == "y"
+    start
+  else
+    puts "Not hungry enough? See you next time!"
+    exit
   end
+
+  def display_pizza_joint(pizza_joint)
+    puts "**** #{pizza_joint.name} ****"
+    puts " "
+    puts "Neighborhood:    #{pizza_joint.neighborhood}"
+    puts "Location:        #{[pizza_joint.location]}"
+    puts "Website:         #{pizza_joint.website_url}"
+
+    puts "****Want More?****"
+    puts " "
+    puts "#{pizza_joint.want_more?}"
+  end
+
+
+end
 end
