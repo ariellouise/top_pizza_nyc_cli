eb01e10de49ce4513d09ca4b093ff9b8f70928b4
Lauren Feld <lauren.feld@aol.com>
2017-10-16 18:22:54 -0400

    Updates to config/environment.rb
    Moved some things around to appear more readable

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
+require_relative "../lib/top_pizza_nyc/pizza"
+require_relative "../lib/top_pizza_nyc/scraper"
+require_relative "../lib/top_pizza_nyc/cli"
