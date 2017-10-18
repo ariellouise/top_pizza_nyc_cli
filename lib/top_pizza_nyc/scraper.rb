3d58f3b2c63facd1cc109b2e4fd5dadff1f8ce61
Lauren Feld <lauren.feld@aol.com>
2017-10-18 15:47:08 -0400

    Isolated description
    Added the correct selector for the description attribute.

--- lib/top_pizza_nyc/scraper.rb
+++ lib/top_pizza_nyc/scraper.rb
@@ -12,10 +12,11 @@ class TopPizzaNYC::Scraper
      restaurant = TopPizzaNYC::Pizza.new() # restaurant is currently an empty pizza object
 
      # attributes
-      #restaurant.name = pizza_box.css("h1").text
+      restaurant.name = pizza_box.css("h1").text
       restaurant.neighborhood = pizza_box.css("p").text.split(/Neighborhood:|Address:/)[1] #grabs neighborhood from text
       restaurant.address = pizza_box.css("a").text.split('.')[0] #grabs address via #split with a '.' to separate url from the address
-      restaurant.url = pizza_box.css("a").text.split('.')[1,2].join('.') || "None Listed" #joining the url array or givng the none listed option.
+      restaurant.url = pizza_box.css("a").text.split('.')[1,2].join('.') #joining the url array or givng the none listed option.
+      restaurant.description = pizza_box.css("p").text.split(/.com|n\/a/)[1]
     end
   end
 
