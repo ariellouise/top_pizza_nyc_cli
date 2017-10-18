d9d410e71dbc7c0ec9da24fb0d3f857040fdaec5
Lauren Feld <lauren.feld@aol.com>
2017-10-18 15:41:19 -0400

    Added ternary statement.
    Added ternary statement for website(url) attribute.

--- lib/top_pizza_nyc/cli.rb
+++ lib/top_pizza_nyc/cli.rb
@@ -1,5 +1,4 @@
 class TopPizzaNYC::CLI
-
   def call
     TopPizzaNYC::Scraper.scrape_pizza
     puts "Welcome to the 25 Best Pizza Places in NYC. I hope you brought your appetite!"
@@ -26,16 +25,16 @@ class TopPizzaNYC::CLI
   end
 
   def display_restaurant(restaurant)
-    puts restaurant
     puts "---------#{restaurant.name}-----------"
     puts
     puts "Neighborhood:    #{restaurant.neighborhood}"
-    puts "Address:     #{restaurant.address}"
-    puts "Website:         #{restaurant.url}"
+    puts "Address:          #{restaurant.address}"
+    puts "Website:          #{!restaurant.url.empty? ? restaurant.url : 'None Listed.'}"
     puts
 
-    puts "---------Want More Info? Enter Y or N. Enter EXIT to quit.-------------"
+    puts "---------Want More Info? Enter Y or N. Enter EXIT to quit-------------"
     input = gets.strip.downcase
+
     if input == "y"
       puts "Decription: #{restaurant.description}"
     elsif input == "n"
